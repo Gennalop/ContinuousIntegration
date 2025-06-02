dccbb5c2e1c8090baeae39a03cd2ba99a27df3dd
GilsonPonce <giljeffpobi@gmail.com>
2025-06-02 14:47:09 -0500

    prueba de jenkis dos

--- gym.py
+++ gym.py
@@ -5,7 +5,6 @@ class GymMembershipSystem:
             'Premium': 80,
             'Family': 120
         }
-        #comentario
         self.additional_features = {
             'Personal Training': 30,
             'Group Classes': 20
